70f83f9c2d82200dcf68fa000d751ab36fb08d5d
shiva23-byte <227527108+shiva23-byte@users.noreply.github.com>
2026-06-02 11:47:54 +0530

    Allow VITE_API_URL environment variable for deployment

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -5,7 +5,9 @@ export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
 
-export const API_BASE = 'http://localhost:5000/api';
+export const API_BASE = import.meta.env.VITE_API_URL 
+  ? `${import.meta.env.VITE_API_URL}/api` 
+  : 'http://localhost:5000/api';
 
 export const CATEGORY_COLORS = {
   Education: '#0984e3',
